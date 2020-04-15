a1c32b10f0daecdba16a86688e72c6f9d87755a6
Wing Chau <wing@devtography.com>
2020-04-16 00:57:47 +0800

    Finishable queue
    - Implemented class `FinisheableQueue(object)` to indicate if the queue has timeout or is finished.

--- source/ib/finishable_queue.py
+++ source/ib/finishable_queue.py
@@ -1,4 +1,40 @@
+import queue
+
 # Queue status
 STARTED = 103
 FINISHED = 200
 TIMEOUT = 408
+
+class FinishableQueue(object):
+    def __init__(self, queue_to_finish: queue.Queue):
+        self.__queue = queue_to_finish
+        self.__status = STARTED
+
+    def get(self, timeout: int):
+        """"
+        Returns a list of queue elements once timeout is finished, or a FINISHED flag is received in the queue
+        :param timeout: how long to wait before giving up
+        :return: list of queue elements
+        """
+        contents_of_queue = []
+        finished = False
+
+        while not finished:
+            try:
+                current_element = self.__queue.get(timeout=timeout)
+
+                if current_element is FINISHED:
+                    finished = True
+                    self.__status = FINISHED
+                else:
+                    contents_of_queue.append(current_element)
+                    # then keep going and try and get more data
+
+            except queue.Empty:
+                finished = True
+                self.__status = TIMEOUT
+
+        return contents_of_queue
+
+    def get_status(self):
+        return self.__status

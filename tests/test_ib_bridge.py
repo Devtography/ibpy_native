adbeef013a41ab0395914f3a55afaf261cdb74d6
Wing Chau <wing@devtography.com>
2020-04-22 13:57:16 +0800

    Expanded test coverage
    - Expanded test coverage on connection related functions defined in `bridge.py`

--- tests/test_ib_bridge.py
+++ tests/test_ib_bridge.py
@@ -2,11 +2,28 @@ from source.ib import IBBridge
 
 import unittest
 
+TEST_PORT = 4002
+TEST_ID = 1001
+
 class TestIBBridge(unittest.TestCase):
     
     def test_init_auto_connect(self):
-        bridge = IBBridge(port=4002, client_id=1001)
+        bridge = IBBridge(port=TEST_PORT, client_id=TEST_ID)
         
-        self.assertTrue(bridge.is_connected)
+        self.assertTrue(bridge.is_connected())
         
         bridge.disconnect()
+
+    def test_init_manual_connect(self):
+        bridge = IBBridge(port=TEST_PORT, client_id=TEST_ID, auto_conn=False)
+        bridge.connect()
+
+        self.assertTrue(bridge.is_connected())
+
+        bridge.disconnect()
+
+    def test_disconnect_without_connection(self):
+        bridge = IBBridge(port=TEST_PORT, client_id=TEST_ID, auto_conn=False)
+        bridge.disconnect()
+
+        self.assertFalse(bridge.is_connected())

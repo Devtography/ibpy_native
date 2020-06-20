cd5020eb2711bad2f2ecd01f3ad8f695764f6b39
Wing Chau <wing@devtography.com>
2020-06-20 18:10:57 +0000

    Handles `tickByTick` data responses
    - Overrides functions `tickByTickAllLast`, `tickByTickBidAsk`, `tickByTickMidPoint` in `IBWrapper` to handles the data responses for `client.reqTickByTickData`.
    - Implemented unit tests in `TestIBWrapper` but not yet tested because of markets are closed on weekends.

--- tests/test_ib_wrapper.py
+++ tests/test_ib_wrapper.py
@@ -1,6 +1,7 @@
 """
 Unit tests for module `ibpy_native.wrapper`.
 """
+import os
 import enum
 import threading
 import unittest
@@ -9,6 +10,7 @@ from ibpy_native import client, wrapper
 from ibpy_native.finishable_queue import FinishableQueue, Status
 from ibapi.contract import Contract
 from ibapi.wrapper import (
+    HistoricalTick, HistoricalTickBidAsk, HistoricalTickLast,
     ListOfHistoricalTick, ListOfHistoricalTickBidAsk, ListOfHistoricalTickLast
 )
 
@@ -18,6 +20,7 @@ class Const(enum.IntEnum):
     """
     RID_RESOLVE_CONTRACT = 43
     RID_FETCH_HISTORICAL_TICKS = 18001
+    RID_REQ_TICK_BY_TICK_DATA = 19001
     QUEUE_MAX_WAIT_SEC = 10
 
 class TestIBWrapper(unittest.TestCase):
@@ -25,9 +28,12 @@ class TestIBWrapper(unittest.TestCase):
     Unit tests for class `IBWrapper`.
     """
     __contract = Contract()
-    __contract.secType = "STK"
-    __contract.symbol = "AAPL"
-    __contract.exchange = "SMART"
+    # __contract.secType = 'STK'
+    # __contract.symbol = 'AAPL'
+    # __contract.exchange = 'SMART'
+    __contract.secType = 'FUT'
+    __contract.symbol = 'YM'
+    __contract.lastTradeDateOrContractMonth = '202009'
     __contract.currency = "USD"
 
     @classmethod
@@ -35,7 +41,11 @@ class TestIBWrapper(unittest.TestCase):
         cls.wrapper = wrapper.IBWrapper()
         cls.client = client.IBClient(cls.wrapper)
 
-        cls.client.connect('127.0.0.1', 4002, 1001)
+        cls.client.connect(
+            os.getenv('IB_HOST', '127.0.0.1'),
+            int(os.getenv('IB_PORT', '4002')),
+            1001
+        )
 
         thread = threading.Thread(target=cls.client.run)
         thread.start()
@@ -119,6 +129,74 @@ class TestIBWrapper(unittest.TestCase):
         self.assertNotEqual(f_queue.get_status(), Status.TIMEOUT)
         self.assertEqual(len(result), 2)
         self.assertIsInstance(result[0], ListOfHistoricalTickLast)
+
+    def test_tick_by_tick_all_last(self):
+        """
+        Test overridden function `tickByTickAllLast`.
+        """
+        f_queue = FinishableQueue(self.wrapper.get_request_queue(
+            Const.RID_REQ_TICK_BY_TICK_DATA
+        ))
+
+        self.client.reqTickByTickData(
+            reqId=Const.RID_REQ_TICK_BY_TICK_DATA.value,
+            contract=self.resolved_contract,
+            tickType='Last',
+            numberOfTicks=0,
+            ignoreSize=True
+        )
+
+        for tick in f_queue.stream():
+            self.assertIsInstance(tick, HistoricalTickLast)
+            break
+
+        self.client.cancelTickByTickData(Const.RID_REQ_TICK_BY_TICK_DATA.value)
+
+    def test_tick_by_tick_bid_ask(self):
+        """
+        Test overridden function `tickByTickBidAsk`.
+        """
+        f_queue = FinishableQueue(self.wrapper.get_request_queue(
+            Const.RID_REQ_TICK_BY_TICK_DATA
+        ))
+
+        self.client.reqTickByTickData(
+            reqId=Const.RID_REQ_TICK_BY_TICK_DATA.value,
+            contract=self.resolved_contract,
+            tickType='BidAsk',
+            numberOfTicks=0,
+            ignoreSize=True
+        )
+
+        for tick in f_queue.stream():
+            self.assertIsInstance(tick, HistoricalTickBidAsk)
+            break
+
+        self.client.cancelTickByTickData(Const.RID_REQ_TICK_BY_TICK_DATA.value)
+
+    def test_tick_by_tick_mid_point(self):
+        """
+        Test overridden function `tickByTickMidPoint`.
+        """
+        queue = self.wrapper.get_request_queue(
+            Const.RID_REQ_TICK_BY_TICK_DATA
+        )
+
+        f_queue = FinishableQueue(queue)
+
+        self.client.reqTickByTickData(
+            reqId=Const.RID_REQ_TICK_BY_TICK_DATA.value,
+            contract=self.resolved_contract,
+            tickType='MidPoint',
+            numberOfTicks=0,
+            ignoreSize=True
+        )
+
+        for tick in f_queue.stream():
+            self.assertIsInstance(tick, HistoricalTick)
+            break
+
+        self.client.cancelTickByTickData(Const.RID_REQ_TICK_BY_TICK_DATA)
 
     @classmethod
     def tearDownClass(cls):

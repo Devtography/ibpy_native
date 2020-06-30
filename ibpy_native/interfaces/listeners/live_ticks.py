43627578b08435321d6e2e353bc7aa169ab933c4
Wing Chau <wing@devtography.com>
2020-06-30 05:26:51 +0000

    Add `on_error` method
    - Added `on_error` method in interface `LiveTicksListener`

--- ibpy_native/interfaces/listeners/live_ticks.py
+++ ibpy_native/interfaces/listeners/live_ticks.py
@@ -3,14 +3,28 @@ Listener interfaces for live market data related functions.
 """
 
 import abc
+from typing import Union
+
+from ibapi.wrapper import (HistoricalTick, HistoricalTickLast,
+                           HistoricalTickBidAsk)
+from ibpy_native.error import IBError
 
 class LiveTicksListener(metaclass=abc.ABCMeta):
     """
     Interface of listener for "Tick-by-Tick Data" related functions.
     """
     @abc.abstractmethod
-    def on_tick_receive(self, req_id: int, tick):
+    def on_tick_receive(self, req_id: int, tick: Union[
+            HistoricalTick, HistoricalTickLast, HistoricalTickBidAsk
+        ]):
         """
         Callback when receives new live tick records.
+        """
+        return NotImplemented
+
+    @abc.abstractmethod
+    def on_error(self, err: IBError):
+        """
+        Callback when encounters errors.
         """
         return NotImplemented

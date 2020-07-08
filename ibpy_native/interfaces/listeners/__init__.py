8fbddc8c268f2ad9fa927653f10361d9fe3b0c2d
Wing Chau <wing@devtography.com>
2020-07-09 01:16:32 +0800

    Implemente interface `BaseListener`
    - Modified `LiveTicksListener` to implement interface `BaseListener` instead of inheriting metaclass `abc.ABCMeta`.
    - Exposes `LiveTicksListener` in module `ibpy_native.interfaces.listeners`.

--- ibpy_native/interfaces/listeners/__init__.py
+++ ibpy_native/interfaces/listeners/__init__.py
@@ -1,2 +1,4 @@
+"""Interfaces of event listeners.
+"""
+from .live_ticks import LiveTicksListener
 from .notification import NotificationListener
-
--- ibpy_native/interfaces/listeners/live_ticks.py
+++ ibpy_native/interfaces/listeners/live_ticks.py
@@ -1,37 +1,33 @@
-"""
-Listener interfaces for live market data related functions.
-"""
+"""Listener interfaces for live market data related functions."""
 
 import abc
 from typing import Union
 
-from ibapi.wrapper import (HistoricalTick, HistoricalTickLast,
-                           HistoricalTickBidAsk)
-from ibpy_native.error import IBError
+from ibapi.wrapper import (HistoricalTick, HistoricalTickBidAsk,
+                           HistoricalTickLast)
 
-class LiveTicksListener(metaclass=abc.ABCMeta):
-    """
-    Interface of listener for "Tick-by-Tick Data" related functions.
-    """
+from .base import BaseListener
+
+class LiveTicksListener(BaseListener):
+    """Interface of listener for "Tick-by-Tick Data" related functions."""
     @abc.abstractmethod
     def on_tick_receive(self, req_id: int, tick: Union[
-            HistoricalTick, HistoricalTickLast, HistoricalTickBidAsk
+            HistoricalTick, HistoricalTickBidAsk, HistoricalTickLast
         ]):
-        """
-        Callback when receives new live tick records.
+        """Callback on receives new live tick records.
+
+        Args:
+            req_id (int): Request identifier (or ticker ID in IB API).
+            tick (Union[HistoricalTick, HistoricalTickBidAsk,
+                HistoricalTickLast]): Tick data received.
         """
         return NotImplemented
 
     @abc.abstractmethod
     def on_finish(self, req_id: int):
-        """
-        Callback when `FINISHED` status is received.
-        """
-        return NotImplemented
+        """Callback on `FINISHED` status is received.
 
-    @abc.abstractmethod
-    def on_error(self, err: IBError):
-        """
-        Callback when encounters errors.
+        Args:
+            req_id (int): Request identifier (or ticker ID in IB API).
         """
         return NotImplemented

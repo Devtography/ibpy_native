45879685e33bc782bea9fb33e5ce20c4b39ae975
Wing Chau <wing@devtography.com>
2020-07-09 04:52:59 +0800

    Unittest `IBClient.stream_live_ticks`
    - Added unittest coverage for `IBClient.stream_live_ticks`.

--- tests/test_ib_client.py
+++ tests/test_ib_client.py
@@ -1,20 +1,27 @@
 """
 Unit tests for module `ibpy_native.client`.
 """
-import os
+import asyncio
 import enum
-import unittest
+import os
 import threading
+import unittest
+
 from datetime import datetime
+from typing import List, Union
 
 import pytz
+
 from ibpy_native.wrapper import IBWrapper
 from ibpy_native.client import IBClient
 from ibpy_native.error import IBError
+from ibpy_native.finishable_queue import Status
+from ibpy_native.interfaces.listeners import LiveTicksListener
 from ibapi.contract import Contract
 from ibapi.wrapper import (
     HistoricalTick, HistoricalTickBidAsk, HistoricalTickLast
 )
+from tests.utils import async_test
 
 class Const(enum.IntEnum):
     """
@@ -25,16 +32,17 @@ class Const(enum.IntEnum):
     RID_RESOLVE_HEAD_TIMESTAMP_EPOCH = 14002
     RID_FETCH_HISTORICAL_TICKS = 18001
     RID_FETCH_HISTORICAL_TICKS_ERR = 18002
+    RID_STREAM_LIVE_TICKS = 19001
 
 class TestIBClient(unittest.TestCase):
     """
     Unit tests for class `IBClient`.
     """
     __contract = Contract()
-    __contract.secType = "STK"
-    __contract.symbol = "AAPL"
-    __contract.exchange = "SMART"
-    __contract.currency = "USD"
+    __contract.secType = 'CASH'
+    __contract.symbol = 'EUR'
+    __contract.exchange = 'IDEALPRO'
+    __contract.currency = 'GBP'
 
     @classmethod
     def setUpClass(cls):
@@ -178,6 +186,69 @@ class TestIBClient(unittest.TestCase):
                 datetime(2020, 5, 20, 3, 20, 0).astimezone(IBClient.TZ),
                 datetime.now().astimezone(IBClient.TZ)
             )
+
+    @async_test
+    async def test_stream_live_ticks(self):
+        """Test function `stream_live_ticks`."""
+        class Listener(LiveTicksListener):
+            """Mock listener for unit test."""
+            ticks: List[Union[
+                HistoricalTick, HistoricalTickBidAsk, HistoricalTickLast
+            ]] = []
+            finished: bool = False
+
+            def on_tick_receive(self, req_id: int, tick: Union[
+                    HistoricalTick, HistoricalTickBidAsk, HistoricalTickLast
+                ]):
+                self.ticks.append(tick)
+
+            def on_finish(self, req_id: int):
+                self.finished = True
+
+            def on_err(self, err: IBError):
+                raise err
+
+        async def cancel_req():
+            await asyncio.sleep(3)
+            self.client.cancelTickByTickData(
+                reqId=Const.RID_STREAM_LIVE_TICKS.value
+            )
+
+            self.client.cancelTickByTickData(reqId=19002)
+
+            queue = self.wrapper.get_request_queue(
+                req_id=Const.RID_STREAM_LIVE_TICKS
+            )
+            queue.put(Status.FINISHED)
+
+        resolved_contract = self.client.resolve_contract(
+            req_id=Const.RID_RESOLVE_CONTRACT.value, contract=self.__contract
+        )
+
+        listener = Listener()
+
+        stream = asyncio.create_task(
+            self.client.stream_live_ticks(
+                req_id=Const.RID_STREAM_LIVE_TICKS.value,
+                contract=resolved_contract,
+                listener=listener,
+                tick_type='BidAsk'
+            )
+        )
+        cancel = asyncio.create_task(cancel_req())
+
+        try:
+            await stream
+            await cancel
+        except IBError as err:
+            tasks = asyncio.all_tasks()
+            for task in tasks:
+                task.cancel()
+
+            raise err
+
+        self.assertGreater(len(listener.ticks), 0)
+        self.assertTrue(listener.finished)
 
     @classmethod
     def tearDownClass(cls):

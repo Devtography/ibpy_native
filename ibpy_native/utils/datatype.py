7621b6d759a558ef319e04eff34eb64835bdb331
Wing Chau <wing@devtography.com>
2021-02-16 11:47:19 +0000

    Add `IBBridge.req_historical_ticks`
    - Added function `req_historical_ticks` and corresponding unit tests.
    - Added data type `datatype.ResHistoricalTicks`.

--- ibpy_native/utils/datatype.py
+++ ibpy_native/utils/datatype.py
@@ -1,10 +1,11 @@
 """Enums/Types for parameters or return objects."""
 import enum
-from typing import List, Union
+from typing import List, NamedTuple, Union
 from typing_extensions import TypedDict
 
 from ibapi import wrapper
 
+#region - Argument options
 @enum.unique
 class EarliestDataPoint(enum.Enum):
     """Data type options defined for earliest data point."""
@@ -19,6 +20,16 @@ class HistoricalTicks(enum.Enum):
     MIDPOINT = "MIDPOINT"
     TRADES = "TRADES"
 
+@enum.unique
+class LiveTicks(enum.Enum):
+    """Data types defined for live tick data."""
+    ALL_LAST = "AllLast"
+    BID_ASK = "BidAsk"
+    MIDPOINT = "MidPoint"
+    LAST = "Last"
+#endregion - Argument options
+
+#region - Return type
 class HistoricalTicksResult(TypedDict):
     """Use to type hint the returns of `IBBridge.get_historical_ticks`."""
     ticks: List[Union[
@@ -28,10 +39,12 @@ class HistoricalTicksResult(TypedDict):
     ]]
     completed: bool
 
-@enum.unique
-class LiveTicks(enum.Enum):
-    """Data types defined for live tick data."""
-    ALL_LAST = "AllLast"
-    BID_ASK = "BidAsk"
-    MIDPOINT = "MidPoint"
-    LAST = "Last"
+class ResHistoricalTicks(NamedTuple):
+    """Return type of function `bridge.IBBridge.get_historical_ticks_v2`."""
+    ticks: List[Union[
+        wrapper.HistoricalTick,
+        wrapper.HistoricalTickBidAsk,
+        wrapper.HistoricalTickLast
+    ]]
+    completed: bool
+#endregion - Return type
